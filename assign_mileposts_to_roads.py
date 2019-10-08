5930ff7edf1596b9df584d13bb97eedf6d559911
Greg Bunce <gbunce@utah.gov>
2019-10-08 12:28:43 -0600

    fix cannot find lrs error

--- assign_mileposts_to_roads.py
+++ assign_mileposts_to_roads.py
@@ -5,7 +5,7 @@ import time
 from arcpy import env
 
 '''
-Notes before running this script:
+*** Notes before running this script ***
 1. Make sure the 'roads_fc' variable is pointing to the desired UTRANS Roads_Edit feature class.
 2. Make sure the 'utrans_conn' variable is pointing to the desired UTRANS database connection file.
 '''
@@ -29,11 +29,11 @@ file = open(text_file_path, "a")
 # global variables
 local_workspace = 'C:/Users/gbunce/Documents/projects/UTRANS/UpdateMilepostsInRoads_Edit/' + 'UpdateUtransMileposts_' + formatted_date + '.gdb'
 arcpy.env.workspace = 'C:/Users/gbunce/Documents/projects/UTRANS/UpdateMilepostsInRoads_Edit/' + 'UpdateUtransMileposts_' + formatted_date + '.gdb'
-#roads_fc = 'Database Connections\\DC_TRANSADMIN@UTRANS@utrans.agrc.utah.gov.sde\\UTRANS.TRANSADMIN.Centerlines_Edit\\UTRANS.TRANSADMIN.Roads_Edit' # make fullpath to utrans
-roads_fc = 'Database Connections\\TestingConnection@utrans.agrc.utah.gov.sde\\UTRANS.TRANSADMIN.Centerlines_Edit\\UTRANS.TRANSADMIN.Roads_Edit'
-lrs_fc ='UDOTRoutes_LRS'
-#utrans_conn = 'Database Connections\\DC_TRANSADMIN@UTRANS@utrans.agrc.utah.gov.sde'
-utrans_conn = 'Database Connections\\TestingConnection@utrans.agrc.utah.gov.sde'
+roads_fc = 'Database Connections\\DC_TRANSADMIN@UTRANS@utrans.agrc.utah.gov.sde\\UTRANS.TRANSADMIN.Centerlines_Edit\\UTRANS.TRANSADMIN.Roads_Edit' # make fullpath to utrans
+# roads_fc = 'Database Connections\\TestingConnection@utrans.agrc.utah.gov.sde\\UTRANS.TRANSADMIN.Centerlines_Edit\\UTRANS.TRANSADMIN.Roads_Edit'
+lrs_fc = 'C:/Users/gbunce/Documents/projects/UTRANS/UpdateMilepostsInRoads_Edit/UpdateUtransMileposts_' + formatted_date + '.gdb/UDOTRoutes_LRS'
+utrans_conn = 'Database Connections\\DC_TRANSADMIN@UTRANS@utrans.agrc.utah.gov.sde'
+# utrans_conn = 'Database Connections\\TestingConnection@utrans.agrc.utah.gov.sde'
 table_output_from_verts = "out_table_from_verts"
 table_output_to_verts = "out_table_to_verts"
 
@@ -100,9 +100,9 @@ def create_new_milepost_values_tables():
     ## this is for road line features but it only honors the direction of the LRS and not the road arc direction.  table_output = arcpy.LocateFeaturesAlongRoutes_lr("roads_dot_rtname_lyr", lrs_fc, route_id_field="LABEL", radius_or_tolerance="0 Meters", out_table="D:/UTRANS/UpdateMilepostsInRoads_Edit/testing.gdb/locate_feat_output", out_event_properties="RID LINE FMEAS TMEAS", route_locations="FIRST", distance_field="DISTANCE", zero_length_events="ZERO", in_fields="FIELDS", m_direction_offsetting="NO_M_DIRECTION")
     # table_output = r"D:/UTRANS/UpdateMilepostsInRoads_Edit/testing.gdb/locate_feat_output"
     print "Begin locate features along route on FromVerts"
-    table_output_from_verts = arcpy.LocateFeaturesAlongRoutes_lr("roads_from_verts_lyr", in_routes="UDOTRoutes_LRS", route_id_field="LABEL", radius_or_tolerance="0 Meters", out_table="out_table_from_verts", out_event_properties="RID POINT MEAS", route_locations="ALL", distance_field="DISTANCE", zero_length_events="ZERO", in_fields="FIELDS", m_direction_offsetting="M_DIRECTON")
+    table_output_from_verts = arcpy.LocateFeaturesAlongRoutes_lr("roads_from_verts_lyr", in_routes=lrs_fc, route_id_field="LABEL", radius_or_tolerance="0 Meters", out_table="out_table_from_verts", out_event_properties="RID POINT MEAS", route_locations="ALL", distance_field="DISTANCE", zero_length_events="ZERO", in_fields="FIELDS", m_direction_offsetting="M_DIRECTON")
     print "Begin locate features along route on ToVerts"
-    table_output_to_verts = arcpy.LocateFeaturesAlongRoutes_lr("roads_to_verts_lyr", in_routes="UDOTRoutes_LRS", route_id_field="LABEL", radius_or_tolerance="0 Meters", out_table="out_table_to_verts", out_event_properties="RID POINT MEAS", route_locations="ALL", distance_field="DISTANCE", zero_length_events="ZERO", in_fields="FIELDS", m_direction_offsetting="M_DIRECTON")
+    table_output_to_verts = arcpy.LocateFeaturesAlongRoutes_lr("roads_to_verts_lyr", in_routes=lrs_fc, route_id_field="LABEL", radius_or_tolerance="0 Meters", out_table="out_table_to_verts", out_event_properties="RID POINT MEAS", route_locations="ALL", distance_field="DISTANCE", zero_length_events="ZERO", in_fields="FIELDS", m_direction_offsetting="M_DIRECTON")
 
     print "Finished Locating Features Along Route at: " + str(datetime.datetime.now())
     file.write("\n" + "Finished Locating Features Along Route at: " + str(datetime.datetime.now()))
