84754f2f5373ef7d214ee3799f6206cce0afcc42
Greg Bunce <gbunce@utah.gov>
2019-12-12 22:12:32 -0700

    quick fix

--- udot_monthly_utrans_dfc_roads_and_highways.py
+++ udot_monthly_utrans_dfc_roads_and_highways.py
@@ -45,13 +45,13 @@ arcpy.MakeFeatureLayer_management(dfc_output_fc, "output_dfc_additions_lyr", "CH
 #: set up join fields
 joinField_current_roads = arcpy.Describe("current_update_roads_lyr").OIDFieldName
 joinField_snapshot_roads = arcpy.Describe("snapshot_base_roads_lyr").OIDFieldName
-joinField_dfc = "UPDATE_FID"
+#joinField_dfc = "UPDATE_FID"
 
 
 #: get the additions
 print("begin joining additions at: " + time.strftime("%c"))
 #: join the current utrans roads to the dfc table to get the new changes
-arcpy.AddJoin_management("current_update_roads_lyr", joinField_current_roads, "output_dfc_additions_lyr", joinField_dfc)
+arcpy.AddJoin_management("current_update_roads_lyr", joinField_current_roads, "output_dfc_additions_lyr", "UPDATE_FID")
 
 #: select the additions
 expression = r"dfc_output_fc.CHANGE_TYPE not in ('NC','D')"
@@ -69,7 +69,7 @@ print("begin joining deletions at: " + time.strftime("%c"))
 arcpy.MakeFeatureLayer_management(dfc_output_fc, "output_dfc_deletions_lyr", "CHANGE_TYPE = 'D'")
 #: copy the selected features to the new feature class - the deletions from the change detection
 #: join the snapshot roads to the dfc table to get the deletions
-arcpy.AddJoin_management("snapshot_base_roads_lyr", joinField_snapshot_roads, "output_dfc_deletions_lyr", joinField_dfc)
+arcpy.AddJoin_management("snapshot_base_roads_lyr", joinField_snapshot_roads, "output_dfc_deletions_lyr", "BASE_FID")
 
 #: select the deletions
 expression = r"dfc_output_fc.CHANGE_TYPE = 'D'"
@@ -81,4 +81,12 @@ print("add deletions to new fc at: " + time.strftime("%c"))
 deletions_fc = dirname + "\\utrans_road_changes_deletions"
 arcpy.CopyFeatures_management(layerName, deletions_fc)
 
+
+#: combine the additions and deletions layers into a new fc named utrans_road_changes
+arcpy.Merge_management([additions_fc, deletions_fc], dirname + "\\utrans_road_changes", "", "NO_SOURCE_INFO")
+
+#: loop through the attribute and spatial/attribute records in the combined layer and compare values with the original values to see what fields have change
+
+
+#: done!
 print("finished script at: " + time.strftime("%c"))
